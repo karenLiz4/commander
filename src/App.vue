aa715d8c9408e398ddf9ab43774433d004b1143d
karenLiz4 <restrepovillegaskarenlizeth@gmail.com>
2023-04-27 21:44:23 +0200

    crear update y delete para la bd

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,11 @@ import { supabase } from './services/supabase'
 // let password = ref('')
 
 //variable productos
-let nombre = ref('')
-let descripcion = ref('')
-let precio = ref('')
-
-//fetch
-// const { data, error } = await supabase
-//   .from('countries')
-//   .select()
-
+let nombre = ref('');
+let descripcion = ref('');
+let precio = ref('');
+let productos = ref([]);
+let selectedProduct = ref(null);
 
 
 //insert
@@ -44,10 +40,57 @@ const submit = async () => {
     .select()
 
   if (error) {
-    console.log(error)
+    console.log(error);
   }
   else {
-    console.log('se ha creado el producto' + nombre.value + 'correctamente')
+    console.log('se ha creado el producto' + nombre.value + 'correctamente');
+  }
+}
+
+
+//fetch
+onMounted(async () => {
+  const { data, error } = await supabase
+    .from('productos')
+    .select()
+
+  if (error) {
+    console.log(error);
+  }
+
+  productos.value = data;
+})
+
+
+//update and delete
+const update = async (producto) => {
+  const { data, error } = await supabase
+    .from('productos')
+    .update({ nombre: producto.nombre, descripcion: producto.descripcion, precio: producto.precio })
+    .eq('id', producto.id)
+    .select()
+
+    
+  if (error) {
+    console.log(error);
+  }
+  else{
+    console.log(data)
+    selectedProduct.value=null;
+  }
+}
+
+const deleteProducto = async (id) => {
+  const { data, error } = await supabase
+    .from('productos')
+    .delete()
+    .eq('id', id)
+
+  if (error) {
+    console.log(error);
+  }
+  else{
+    selectedProduct.value=null;
   }
 }
 
@@ -72,22 +115,75 @@ const submit = async () => {
     <button @click="submit">Registrarse</button> -->
 
 
-      <h1></h1>
-    <div class="form-registrar">
+  <div>
+    <h1>Crear producto</h1>
+    <div class="form-producto">
       <label for="nombre">Nombre de producto:</label>
       <input v-model="nombre" type="text" name="nombre" required />
     </div>
-    <div class="form-registrar">
-      <label for="descripcion">Descripcion:</label>
+    <div class="form-producto">
+      <label for="descripcion">Descripción:</label>
       <input v-model="descripcion" type="text" name="descripcion" required />
     </div>
-    <div class="form-registrar">
+    <div class="form-producto">
       <label for="precio">Precio:</label>
-      <input v-model="precio" type="float"  name="precio" required />
+      <input v-model="precio" type="float" name="precio" required />
     </div>
     <button @click="submit">Aceptar</button>
+  </div>
 
 
+  <div>
+    <h1>Listado de productos</h1>
+    <table v-if="productos">
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Descripción</th>
+          <th>Precio</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="producto in productos" :key="producto.id">
+          <td>{{ producto.nombre }}</td>
+          <td>{{ producto.descripcion }}</td>
+          <td>{{ producto.precio }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div v-else>No hay ningún producto en la base de datos</div>
+  </div>
+
+  <div>
+    <h1>Editar o eliminar</h1>
+    <label for="selectedProduct">Seleccionar producto</label>
+    <select name="selectedProduct" id="selectedProduct" v-model="selectedProduct">
+      <option v-for="producto in productos" :key="producto.id" :value="producto">{{ producto.nombre }}</option>
+    </select>
+    <div v-if="selectedProduct">
+      <div class="form-producto">
+        <label for="selectedNombre">Nombre de producto:</label>
+        <input v-model="selectedProduct.nombre" type="text" name="selectedNombre" required />
+      </div>
+
+      <div class="form-producto">
+        <label for="selectedDescripcion">Descripción:</label>
+        <input v-model="selectedProduct.descripcion" type="text" name="selectedDescripcion" required />
+      </div>
+
+      <div class="form-producto">
+        <label for="selectedPrecio">Precio:</label>
+        <input v-model="selectedProduct.precio" type="float" name="selectedPrecio" required />
+      </div>
+
+      <div>
+      <button  @click="update(selectedProduct)">Actualizar</button>
+      <button  @click="deleteProducto(selectedProduct.id)">Eliminar</button>
+    </div>
+    </div>
+
+    
+  </div>
 </template>
 
 <!-- <style scoped></style> -->
