abf965e1f3b4443906b8907773ec79fd2e629a4e
karenLiz4 <restrepovillegaskarenlizeth@gmail.com>
2023-04-27 20:21:30 +0200

    crear  y conectar bd  y funcion subir productos

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,93 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { onMounted, ref } from 'vue'
+import { supabase } from './services/supabase'
+
+
+//variables usuario
+// let username = ref('')
+// let email = ref('')
+// let password = ref('')
+
+//variable productos
+let nombre = ref('')
+let descripcion = ref('')
+let precio = ref('')
+
+//fetch
+// const { data, error } = await supabase
+//   .from('countries')
+//   .select()
+
+
+
+//insert
+// const submit = async () => {
+//   const { data, error } = await supabase
+//     .from('usuarios')
+//     .insert({ username: username.value, email: email.value, email: password.value })
+//     .select()
+
+//   if (error) {
+//     console.log(error)
+//   }
+//   else {
+//     console.log('se ha creado el usuario' + data + 'correctamente')
+//   }
+// }
+
+
+//insert
+const submit = async () => {
+  const { data, error } = await supabase
+    .from('productos')
+    .insert({ nombre: nombre.value, descripcion: descripcion.value, precio: precio.value })
+    .select()
+
+  if (error) {
+    console.log(error)
+  }
+  else {
+    console.log('se ha creado el producto' + nombre.value + 'correctamente')
+  }
+}
+
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <!-- formulario registro
+    <h1>Registrar</h1>
+    <div class="form-registrar">
+      <label for="username">Nombre de Usuario</label>
+      <input v-model="username" type="text" name="username" required />
     </div>
-  </header>
+    <div class="form-registrar">
+      <label for="email">Correo Electrónico</label>
+      <input v-model="email" type="email" name="email" required />
+    </div>
+    <div class="form-registrar">
+      <label for="password">Contraseña</label>
+      <input v-model="password" type="password"  name="password" required />
+    </div>
+    <button @click="submit">Registrarse</button> -->
 
-  <main>
-    <TheWelcome />
-  </main>
+
+      <h1></h1>
+    <div class="form-registrar">
+      <label for="nombre">Nombre de producto:</label>
+      <input v-model="nombre" type="text" name="nombre" required />
+    </div>
+    <div class="form-registrar">
+      <label for="descripcion">Descripcion:</label>
+      <input v-model="descripcion" type="text" name="descripcion" required />
+    </div>
+    <div class="form-registrar">
+      <label for="precio">Precio:</label>
+      <input v-model="precio" type="float"  name="precio" required />
+    </div>
+    <button @click="submit">Aceptar</button>
+
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<!-- <style scoped></style> -->
