93121d0184141b578b08a55d6a0cbbc631fc4d59
karenLiz4 <restrepovillegaskarenlizeth@gmail.com>
2023-05-26 17:40:56 +0200

    mas info comanda y num de productos para imprimir

--- src/pages/Comanda.vue
+++ src/pages/Comanda.vue
@@ -12,6 +12,8 @@ const emit = defineEmits(['crearComanda'])
 let comanda = ref({
   camarero: '',
   nMesa: '',
+  comensales: '',
+  nota: '',
   producto: ''
 })
 
@@ -25,20 +27,37 @@ const fetchProductos = async () => {
   productos.value = data
 }
 
+
+
+//agregar nombre al array
 const agregarNombreComanda = (nombre) => {
   nombreBotonesSeleccinados.value.push(nombre)
 }
 
+//eliminar nombre del array
+const eliminarNombreComanda = (index) => {
+  nombreBotonesSeleccinados.value.splice(index, 1);
+}
 
 //insert
 const submitComanda = async () => {
-  //guarda los productos de la comanda en la base de datos y los concatena
-  const productosSeleccionados = nombreBotonesSeleccinados.value.join(', ')
+  //guarda los productos de la comanda en la base de datos y los concatena separados por comas
+  const productosSeleccionados = nombreBotonesSeleccinados.value.join(',')
   comanda.value.producto = productosSeleccionados
+
+
+  /*Al utilizar el conjunto (Set) y la función map, se obtiene una lista única de nombres de productos
+   sin repetición en nombreBotonesSeleccinados.value. Luego, se calcula la cantidad de cada producto y se
+   genera el HTML con la cantidad y el nombre de cada producto en la variable productosComandaHtml.
+   (chatGPT)*/
+  const productosComandaHtml = [...new Set(nombreBotonesSeleccinados.value)].map((nombre) => {
+    const cantidad = nombreBotonesSeleccinados.value.filter((n) => n === nombre).length;
+    return `<h3>(x${cantidad}) ${nombre}</h3>`;
+  }).join('');
 
   const { data, error } = await supabase
     .from('comandas')
-    .insert({ camarero: comanda.value.camarero, nMesa: comanda.value.nMesa, producto: comanda.value.producto })
+    .insert({ camarero: comanda.value.camarero, nMesa: comanda.value.nMesa, comensales: comanda.value.comensales, nota: comanda.value.nota, producto: comanda.value.producto })
     .select()
 
   if (error) {
@@ -47,25 +66,57 @@ const submitComanda = async () => {
   else {
     console.log('se ha creado la comanda' + data + 'correctamente');
 
-    const comandaTxt = `Camarero: ${comanda.value.camarero}\nNúmero de mesa: ${comanda.value.nMesa}\nProductos: ${comanda.value.producto}\n\n`;
+    const comandaHtml = `
+      <html>
+      <head>
+        <title>Commander</title>
+        <style>
 
-    // Crear el contenido del archivo
-    const fileContent = new Blob([comandaTxt], { type: 'text/plain' });
+        h1 {
+          font-family: 'Kaushan Script', cursive;
+          font-size: 3.5rem;
+          font-weight: bold;
+        }
 
-    // Crear un enlace de descarga
-    const downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(fileContent);
-    downloadLink.download = 'comanda.txt';
+        h3 {
+          font-family: 'Kaushan Script', cursive;
+          font-size: 1.0rem;
+        }
 
-    // Simular clic en el enlace para descargar el archivo
-    downloadLink.click();
+        p {
+          font-family: 'Kaushan Script', cursive;
+        }
+        </style>
+      </head>
+      <body>
+        <h1>Comanda </h1>
+        <h3>Camarero:</h3> <p>${comanda.value.camarero}</p>
+        <h3>Número de mesa:</h3> <p>${comanda.value.nMesa}</p>
+        <h3>Número de comensales:</h3> <p>${comanda.value.comensales}</p>
+        <h3>Nota:</h3> <p>${comanda.value.nota}</p>
+        <h3>Productos:</h3> <p>${productosComandaHtml}</p>
+      </body>
+      </html>
+    `;
 
+    // Crear una nueva ventana del navegador con la comanda HTML
+    const printWindow = window.open('', '_blank');
+    printWindow.document.open();
+    printWindow.document.write(comandaHtml);
+    printWindow.document.close();
 
+    // Imprimir la comanda
+    printWindow.print();
+
+    //cerrar la ventana
+    printWindow.close();
 
     //dejar los campos vacios
     comanda.value = {
       camarero: '',
       nMesa: '',
+      comensales: '',
+      nota: '',
       producto: ''
     };
     nombreBotonesSeleccinados.value = [];
@@ -82,14 +133,9 @@ onMounted(() => {
 // Exponer las funciones para poder llamarlas desde la plantilla
 defineExpose({
   fetchProductos,
-  agregarNombreComanda
+  agregarNombreComanda,
+  eliminarNombreComanda
 })
-
-// Método para imprimir la página
-// const printWindow = () => {
-//   window.print();
-// }
-
 
 
 
@@ -159,17 +205,26 @@ defineExpose({
           <div class="card-body">
             <div class="form-floating mb-3">
               <input v-model="comanda.camarero" type="text" class="form-control" name="camarero" required />
-              <label for="camarero">Camarero:</label>
+              <label for="camarero">Camarero: (obligatorio)</label>
             </div>
             <div class="form-floating mb-3">
               <input v-model="comanda.nMesa" type="text" class="form-control" name="nMesa" required />
-              <label for="nMesa">Número de mesa:</label>
+              <label for="nMesa">Número de mesa: (obligatorio)</label>
             </div>
             <div class="form-floating mb-3">
-              <!-- <textarea v-model="comanda.producto" type="text" class="form-control" name="producto" required /> -->
+              <input v-model="comanda.comensales" type="text" class="form-control" name="comensales" required />
+              <label for="nMesa">Número de comensales: (obligatorio)</label>
+            </div>
+            <div class="form-floating mb-3">
+              <input v-model="comanda.nota" type="text" class="form-control" name="nota" />
+              <label for="nMesa">Nota: (opcional)</label>
+            </div>
+            <div class="form-floating mb-3">
               <div v-if="nombreBotonesSeleccinados">
-                <div v-for="nombreBotonSeleccinado in nombreBotonesSeleccinados">
-                  <h4 class="text-center">{{ nombreBotonSeleccinado }}</h4>
+                <div v-for="(nombreBotonSeleccinado, index) in nombreBotonesSeleccinados" :key="index"
+                  class="d-flex justify-content-between">
+                  <h4>{{ nombreBotonSeleccinado }}</h4>
+                  <button class="btn btn-danger mb-2" @click="eliminarNombreComanda(index)">Eliminar</button>
                 </div>
                 <button class="btn btn-block mb-2" @click="submitComanda()">Aceptar</button>
               </div>
@@ -198,5 +253,18 @@ defineExpose({
   padding: 60px 50px;
   background-color: white;
   box-shadow: 20px 20px 80px rgb(206, 206, 206)
+}
+
+.btn.btn-danger {
+  border-radius: 20px;
+  border-color: red;
+  background-color: red;
+  color: #fff;
+
+}
+
+.btn.btn-danger:hover {
+  background-color: #CC0000;
+  color: #fff;
 }
 </style>
